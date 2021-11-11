140efe89100ea31d85219f4cf0c6cce33bd1b96e
ayoubmn <ayoubmn.1999n1@gmail.com>
2021-11-11 11:13:44 +0000

    convert to php

---
+++ public/jalon1/src/login.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Netflex</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <!-- Bootstrap icons-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet" />
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+    <!-- MDB -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.6.0/mdb.min.css" rel="stylesheet" />
+
+
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/styles.css" rel="stylesheet" />
+    <link href="css/index.css" rel="stylesheet" />
+
+    <?php 
+        $str = file_get_contents("data.json"); 
+        $json = json_decode($str,true);
+
+    ?>
+
+</head>
+
+<body>
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container px-4 px-lg-5">
+            <a class="navbar-brand" href="index.php">NetFlex</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">Category</a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#!">Action</a></li>
+                            <li><a class="dropdown-item" href="#!">Comedy</a></li>
+                            <li><a class="dropdown-item" href="#!">Horror</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item"><a class="nav-link" href="#!">Favoris</a></li>
+                </ul>
+                <!-- Search form -->
+                <div class="input-group ps-5">
+                    <div id="navbar-search-autocomplete" class="form-outline">
+                        <input type="search" id="form1" class="form-control" />
+                        <label class="form-label" for="form1">Search</label>
+                    </div>
+                    <button type="button" class="btn btn-primary">
+                        <i class="fas fa-search"></i>
+                    </button>
+                </div>
+
+                <!--login form-->
+                <form class="d-flex">
+                    <button class="btn btn-outline-dark" type="submit">
+                        <a href="login.php" style="text-decoration:none;" >Login/Signup</a>
+                    </button>
+                </form>
+            </div>
+        </div>
+    </nav>
+
+
+        <div id="login">
+            <h3 class="text-center text-white pt-5">Se connecter</h3>
+            <div class="container">
+                <div id="login-row" class="row justify-content-center align-items-center">
+                    <div id="login-column" class="col-md-6">
+                        <div id="login-box" class="col-md-12">
+                            <form id="login-form" class="form" action="" method="post">
+                                <h3 class="text-center text-info">Se connecter</h3>
+                                <div class="form-group mb-2">
+                                    <label for="email" class="text-info">Email</label><br>
+                                    <input type="text" name="username" id="email" class="form-control">
+                                </div>
+                                <div class="form-group mb-4">
+                                    <label for="password" class="text-info">Mot de passe</label><br>
+                                    <input type="text" name="password" id="password" class="form-control">
+                                </div>
+                                <div id="register-link" class="text-right">
+                                    Vous n'êtes pas encore inscrits ? Inscrivez-vous<a href="#" class="text-info"> ici</a>.
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
+</body>
+
+</html>
