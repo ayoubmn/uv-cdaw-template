979e98d44d79fa62edeb7e5b85509444611000e4
ayoubmn <ayoubmn.1999n1@gmail.com>
2021-11-15 15:04:34 +0000

    jalon2 init

---
+++ public/jalon2/catalogue/routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    #return "<h2>hello world</h2>";
+    #echo <h2>hello world</h2>
+    
+    return view('welcome');
+});
+
+
+Route::get('/nom/{nom}/{prenom}', function ($nom,$prenom) {
+    return "Bonjour $nom $prenom";
+});
+
+Route::get('/title/{title}', function ($title) {
+    return "$title";
+})->where(['title' => '[a-z]+']);
