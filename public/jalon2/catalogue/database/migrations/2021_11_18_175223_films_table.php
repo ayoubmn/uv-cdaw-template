ba90afea04bd2ba7b208de56840db5da3d79fb23
ayoubmn <ayoubmn.1999n1@gmail.com>
2021-11-18 18:47:40 +0000

    film CRUD api

--- public/jalon2/catalogue/database/migrations/2021_11_18_175223_films_table.php
+++ public/jalon2/catalogue/database/migrations/2021_11_18_175223_films_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFilmsTable extends Migration
+class FilmsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -18,6 +18,8 @@ class CreateFilmsTable extends Migration
             $table->string('name');
             $table->foreignId('category');
             $table->longText('url');
+            $table->timestamps();
+
         });
     }
 
--- public/jalon2/catalogue/routes/api.php
+++ public/jalon2/catalogue/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Film;
+use App\Models\Category;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,40 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('film', function (Request $request) {
+    $film = Film::create($request->all());
+    $cat = Category::find($request->category);
+    $film->category=$cat->id;
+    $film->save();
+    return response()->json(["objet" => $film, "msg" => "Ajouté avec succès"]);
+});
+
+Route::get('film', function () {
+    $film = Film::all();
+    return response()->json(["objet" => $film]);
+});
+
+Route::delete('film/{id}', function ($id) {
+    $film = Film::find($id);
+    $film->delete();
+    return response()->json("Supprimé avec succès");
+});
+
+Route::put('film/{id}', function (Request $request, $id) {
+    $film = Film::find($id);
+
+    if ($request->has('name')) {
+        $film->name = $request->input('name');
+    }
+    if ($request->has('category')) {
+        $film->category = $request->input('category');
+    }
+    if ($request->has('url')) {
+        $film->url = $request->input('url');
+    }
+
+    $film->save();
+    return response()->json("Modifié avec succès");
 });
